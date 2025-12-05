5836491c87fe8d4968bfad5174b411c1e2d214e1
adb1425 <belcheralexander@gmail.com>
2025-12-05 01:05:06 -0500

    Going to bed

--- wip.py
+++ wip.py
@@ -15,5 +15,14 @@ team_list = pd.read_csv('team_list.csv')
 # +++++++++++++++++++++++++++++++++++++++++++++++++
 tl = team_list.sort_values(by ='Team')
 
-print(tl[['Sport','Team'][0]])
+# Displays the sport and team name, sorted by team name
+print(tl[['Sport','Team'][0:5]])
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++
+# Display the players on a specific team
+# +++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Uses the previous print function
+selection = input("Please type a team to see their All-Time starting 5: ")
+
+I think to display the players on the team I check to see if the index value contains the team name. I think that should work, just have to figure out the syntax.
